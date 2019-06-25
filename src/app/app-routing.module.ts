8ebd3e586be18765b6c2db2a68a7d591774e9d4b
Luis Vilca <ferluisxd@gmail.com>
2019-06-25 15:45:17 -0500

    toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'player',
+    loadChildren:'./player/player.module#PlayerModule'
+},
+{
+    path: 'team',
+    loadChildren:'./team/team.module#TeamModule'
+},
+{
+    path: 'tournament',
+    loadChildren:'./tournament/tournament.module#TournamentModule'
+},
+{
+  path: 'statistics',
+  loadChildren:'./statistics/statistics.module#StatisticsModule'
+},
+{
+  path: 'match',
+  loadChildren:'./match/match.module#MatchModule'
+},
+{
+    path:'',
+    redirectTo: '',
+    pathMatch: 'full'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +18,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    //Material
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
