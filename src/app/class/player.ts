f25c15340ef12c3e4f3a656210507a612af43219
Luis Vilca <ferluisxd@gmail.com>
2019-06-25 08:31:58 -0500

    class done

--- src/app/class/player.ts
+++ src/app/class/player.ts
@@ -1,2 +1,6 @@
 export class Player {
+    id:number
+    name:String
+    gamePreference:String
+    teamId:number
 }
--- src/app/class/team.ts
+++ src/app/class/team.ts
@@ -1,2 +1,6 @@
 export class Team {
+    id:number
+    name:String
+    nMembers:number
+    tournamentId:number
 }
