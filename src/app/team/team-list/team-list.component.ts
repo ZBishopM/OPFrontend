b3102861e06b65ff0fd72eafe144f6f4d14c59ee
pieroqb28 <u201710398@upc.edu.pe>
2019-06-28 22:28:50 -0500

    team up

--- src/app/team/team-list/team-list.component.ts
+++ src/app/team/team-list/team-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TeamCreateComponent } from '../team-create/team-create.component';
+import { MatDialog, MatTableDataSource } from '@angular/material';
+import { MatSort} from '@angular/material/sort';
+import { TeamService } from '../team.service';
+import { Team } from 'src/app/class/team';
 
 @Component({
   selector: 'app-team-list',
@@ -6,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team-list.component.css']
 })
 export class TeamListComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'nmembers', 'tournament'];
+  dataSource:any = []
 
-  constructor() { }
+  constructor(public dialog:MatDialog,private teamService:TeamService) { }
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   ngOnInit() {
+    this.listData()
+  }
+  listData(){
+    this.teamService.getTeams().subscribe(data=>{this.dataSource = new MatTableDataSource(data)})
+  }
+  applyFilter(filterValue: string) {
+    console.log(filterValue)
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  openCreate(){
+    const dialog = this.dialog.open(TeamCreateComponent,{
+      width:'700px',
+      data: {}
+    })
+    dialog.afterClosed().subscribe(result=>{
+      console.log(result)
+      this.listData()
+    })
   }
 
 }
--- src/app/team/team-routing.module.ts
+++ src/app/team/team-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TeamListComponent } from './team-list/team-list.component';
-
+import { TeamCreateComponent } from './team-create/team-create.component';
 const routes: Routes = [
   {
     path:'',
     component: TeamListComponent
+  },
+  {
+    path:'new',
+    component:TeamCreateComponent
   }
 ];
 
