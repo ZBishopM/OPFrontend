b80c8bf91dab7d050f521bd261dc5a3dbc93f8d2
Luis Vilca Flores <ferluisxd@gmail.com>
2019-06-29 00:09:37 -0500

    match "working"

---
+++ src/app/match/match.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Match } from '../class/match';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MatchService {
+  apiUrl = 'http://localhost:8080/api/match'
+
+  constructor(private http: HttpClient) { }
+
+  getMatchs(){
+    return this.http.get<Match[]>(this.apiUrl);
+  }
+  postMatch(data:Match){
+    return this.http.post<Match>(this.apiUrl,data)
+  }
+  putMatch(data:Match){
+    return this.http.put<Match>(this.apiUrl,data)
+  }
+}
